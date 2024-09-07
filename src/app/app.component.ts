d6879b89542211d0e110bd15cac7ba0a96e5fe76
leticiaschneider <leticia.schneider@gmail.com>
2024-09-07 18:22:23 -0300

    defining header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'bookshelf';
-  constructor(private router: Router) {
-    console.log('Router is', this.router);
-  }
 }
