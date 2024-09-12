57adc572f6ee171c151008e1ec5c36c280f5930d
leticiaschneider <leticia.schneider@gmail.com>
2024-09-12 20:41:34 -0300

    add new components for pages

---
+++ src/app/features/my-library/my-library.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-library',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-library.component.html',
+  styleUrl: './my-library.component.scss'
+})
+export class MyLibraryComponent {
+
+}
