542cf1583f639c6eab38266a6dc5ca0521dd22da
leticiaschneider <leticia.schneider@gmail.com>
2024-09-24 16:02:00 -0300

    populate table with books data from localStorage

--- src/app/features/my-library/my-library.component.ts
+++ src/app/features/my-library/my-library.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-library',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-library.component.html',
   styleUrl: './my-library.component.scss'
 })
-export class MyLibraryComponent {
+export class MyLibraryComponent implements OnInit {
 
+  bookList: any[] = [];
+
+  ngOnInit(): void {
+    this.loadStoredBooks();
+  }
+
+  loadStoredBooks() {
+    if (typeof window !== 'undefined' && localStorage) {
+      const storedBooks = localStorage.getItem('storedBooks');
+      if (storedBooks) {
+        this.bookList = JSON.parse(storedBooks);
+        this.bookList.forEach(book => console.log('Book: ', book));
+
+        // console.log('livroooss>: ', JSON.stringify(this.bookList, null, 2));
+      }
+    }
+  }
 }
