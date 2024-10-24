657ac28a52bdd59ea279b174121511691a898875
leticiaschneider <leticia.schneider@gmail.com>
2024-10-24 20:18:25 -0300

    implemented delete function

--- src/app/features/my-library/my-library.component.ts
+++ src/app/features/my-library/my-library.component.ts
@@ -14,6 +14,8 @@ export class MyLibraryComponent implements OnInit {
   dropdownOptions: string[] = ['All', 'Read', 'Reading', 'Want to Read', 'Rereading', 'Abandoned'];
 
   bookList: any[] = [];
+  showModal: boolean = false;
+  bookToDelete: any = null;
 
   ngOnInit(): void {
     this.loadStoredBooks();
@@ -31,5 +33,23 @@ export class MyLibraryComponent implements OnInit {
 
   selectOption(option: string) {
     this.selectedOption = option;
+  }
+
+  openModal(book: any) {
+    this.showModal = true;
+    this.bookToDelete = book;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  deleteItem() {
+    if (this.bookToDelete) {
+      this.bookList = this.bookList.filter(book => book.title !== this.bookToDelete.title);
+      localStorage.setItem('storedBooks', JSON.stringify(this.bookList));
+      console.log('Book deleted:', this.bookToDelete);
+    }
+    this.closeModal();
   }
 }
