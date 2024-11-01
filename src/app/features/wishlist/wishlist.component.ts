c45288e9e1d4cca47b46ca42c7a027c64519f43b
Leticia <leticia.schneider@gmail.com>
2024-10-31 22:33:12 -0300

    Creating wishlist page

--- src/app/features/wishlist/wishlist.component.ts
+++ src/app/features/wishlist/wishlist.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.scss'
 })
-export class WishlistComponent {
+export class WishlistComponent implements OnInit {
+  wishlist: any[] = [];
 
+  ngOnInit(): void {
+    this.loadWishlistBooks();
+  }
+
+  loadWishlistBooks(): void {
+    if (typeof window !== 'undefined' && localStorage) {
+      const storedBooks = localStorage.getItem('storedBooks');
+      if (storedBooks) {
+        const bookList = JSON.parse(storedBooks);
+        this.wishlist = bookList.filter((book: any) => book.status === 'Wishlist');
+      }
+    }
+  }
+
+  removeFromWishlist(book: any): void {
+    if (typeof window !== 'undefined' && localStorage) {
+      const storedBooks = localStorage.getItem('storedBooks');
+      if (storedBooks) {
+        const bookList = JSON.parse(storedBooks);
+        const updatedBooks = bookList.filter((b: any) => b.title !== book.title);
+        localStorage.setItem('storedBooks', JSON.stringify(updatedBooks));
+        this.wishlist = this.wishlist.filter((b: any) => b.title !== book.title);
+      }
+    }
+  }
 }
